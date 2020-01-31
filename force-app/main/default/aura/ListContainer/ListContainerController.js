ab18e9f3f825472f4c7e4961d87bd5d34503a99c
Isaí Desales <idesales@coca-cola.com>
2020-01-31 13:03:33 -0600

    Added request to controller

--- force-app/main/default/aura/ListContainer/ListContainerController.js
+++ force-app/main/default/aura/ListContainer/ListContainerController.js
@@ -1,5 +1,9 @@
 ({
-    myAction : function(component, event, helper) {
-
+    doInit : function(component, event, helper) {
+        helper.getNotesLists(component,event,function(response){
+            //Si el resultado fue satifactorio agregamos los datos a la vista y así desplegue la informacion a los usuarios
+            component.set('v.data',response.getReturnValue());
+            component.find('container').set('v.isLoading',false);
+        });
     }
 })
--- force-app/main/default/aura/ListContainer/ListContainerHelper.js
+++ force-app/main/default/aura/ListContainer/ListContainerHelper.js
@@ -1,5 +1,34 @@
 ({
-    helperMethod : function() {
+    getColumns : function (component,helper){
+        component.set('v.columns',[
+            {label : 'Nombre', fieldName :'Name',type :'text'},
+            {label : 'Tipo', fieldName :'Type__c',type :'text'},
+            {label : 'Creado por', fieldName :'Owner.Name',type : 'text'},
+            {label : 'Creado por', fieldName :'Owner.Name',type : 'date-local'}
+        ]);
+    },
+    getNotesLists : function(component,event,callback) {
 
+        //Hacemos que aparezca un icono de carga
+        component.find('container').set('v.isLoading',true);
+
+        //realizamos la llamada a nuestro backend para obtener las notas y listas
+        var action = component.get('c.ListController');
+        action.setCallback(this,function(response){
+            if (response.getState() == 'SUCCESS'){
+                callback(response);
+            }else if (response.getState() == 'ERROR'){
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + 
+                                 errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+        });
+        $A.enqueueAction(action);
     }
 })
